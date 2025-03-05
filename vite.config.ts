14857f062ebc2a035630a79c451c02e787aedd3c
Thomas Jacques Bryan Penner <thomas.penner@student.uva.nl>
2025-03-05 20:34:30 +0100

    Feature/api gateway (#8)
    * React+Vite Init
    
    * Basic Welcome Page
    
    * Basic integration of Cognito
    
    * Signup+Login
    
    * Basic GET and POST methods for API dummy
    
    * Unused vars disregarded
    
    * vite config for amplify
    
    * still debugging
    
    ---------
    
    Co-authored-by: tomapnr <thomaspenner00@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // 👈 This is critical for Amplify!
+  base: '/', // This is CRUCIAL for Amplify
 
   plugins: [react()],
 })
