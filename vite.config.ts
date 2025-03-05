d5ec13a0c648609927f6d1baa7f0cddd40960b6d
Mothnep <thomas.penner@student.uva.nl>
2025-03-05 20:27:38 +0100

    still debugging

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // 👈 This is critical for Amplify!
+  base: '/', // This is CRUCIAL for Amplify
   plugins: [react()],
 })
