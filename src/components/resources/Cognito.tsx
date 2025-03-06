7c15a608b3296196f9f1b999599951877d351291
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-03-06 17:23:54 +0100

    env mismach fix

--- src/components/resources/Cognito.tsx
+++ src/components/resources/Cognito.tsx
@@ -2,7 +2,7 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 const poolData = {
     UserPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-    ClientId: import.meta.env.VITE_COGNITO_APP_CLIENT_ID,
+    ClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
 };
 
 export default new CognitoUserPool(poolData);
